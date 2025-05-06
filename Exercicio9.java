9a5129535d94c5820215207b7938705aa2c10cf6
Mikaely19 <125111409306@ulife.com.br>
2025-05-05 23:41:55 -0300

    Update Exercicio9.java

--- Exercicio9.java
+++ Exercicio9.java
@@ -5,24 +5,23 @@ public class App {
         int[] vetor1 = new int[6];
         int[] vetor2 = new int[6];
 
-        System.out.println("Digite os 6 números para o vetor 1:");
+        System.out.println("Digite os números para o vetor 1:");
         for (int i = 0; i < 6; i++) {
             System.out.print("Elemento " + (i + 1) + ": ");
             vetor1[i] = scanner.nextInt();
         }
 
-        System.out.println("\nDigite os 6 números para o vetor 2:");
+        System.out.println("Digite os 6 números para o vetor 2:");
         for (int i = 0; i < 6; i++) {
             System.out.print("Elemento " + (i + 1) + ": ");
             vetor2[i] = scanner.nextInt();
         }
 
-        // Encontrar elementos comuns
         List<Integer> comuns = new ArrayList<>();
-        for (int i = 0; i < vetor1.length; i++) {
-            for (int j = 0; j < vetor2.length; j++) {
+        for (int i = 0; i < vetor1.; i++) {
+            for (int j = 0; j < vetor2; j++) {
                 if (vetor1[i] == vetor2[j]) {
-                    // Para evitar duplicatas na lista de comuns
+
                     if (!comuns.contains(vetor1[i])) {
                         comuns.add(vetor1[i]);
                     }
@@ -30,7 +29,6 @@ public class App {
             }
         }
 
-        // Exibir o vetor de elementos comuns
         System.out.println("\nElementos comuns aos dois vetores:");
         for (int num : comuns) {
             System.out.print(num + " ");
